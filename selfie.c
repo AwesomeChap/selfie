24887eae238ad6b9cbdfd9374c7efc8b3810c389
operator <operator@MacBook-Pro-von-A1417.local>
2023-10-12 18:25:00 +0200

    done print-your-name

--- selfie.c
+++ selfie.c
@@ -12117,6 +12117,8 @@ int main(int argc, char** argv) {
   init_target();
   init_kernel();
 
+  printf("%s: This is Jatin Kumar's Selfie!\n", selfie_name);
+
   exit_code = selfie(0);
 
   return exit_selfie(exit_code, " [ ( -m | -d | -r | -y ) 0-4096 ... ]");
